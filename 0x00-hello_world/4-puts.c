f6e78a4d0891a99a79a49c6acf751d07474242b1
Mohamed Amine Fhal <fhal.mohamed.amine@gmail.com>
2023-06-15 03:10:51 -0700

    first c program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main - This will return a str
+(* a blank line
+ * Description: This fun will print a str using the puts fun)?
+(* section header: Section description)*
+ * Return: just return 0 for ok
+ */
+int main(void)
+{
+char Mystr[52] = "\"Programming is like building a multilingual puzzle";
+puts(Mystr);
+return (0);
+}
