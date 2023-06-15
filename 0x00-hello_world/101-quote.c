5d655547aae62854c4b0c53960d6cb248ac190bb
Mohamed Amine Fhal <fhal.mohamed.amine@gmail.com>
2023-06-15 07:49:18 -0700

    101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main - This will return a str
+(* a blank line
+ * Description: This fun will print a str using the printf fun)?
+(* section header: Section description)*
+ * Return: just return 0 for ok
+ */
+int main(void)
+{
+printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+return (1);
+}
