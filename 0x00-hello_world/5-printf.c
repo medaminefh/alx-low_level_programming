4e028bef8e337b0b904e70d1342b65e8ffa41088
Mohamed Amine Fhal <fhal.mohamed.amine@gmail.com>
2023-06-15 07:20:05 -0700

    use printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main - This will return a str
+(* a blank line
+ * Description: This fun will print a str using the printf fun)?
+(* section header: Section description)*
+ * Return: just return 0 for ok
+ */
+int main(void)
+{
+char Mystr[] = "with proper grammar, but the outcome is a piece of art,";
+printf("%s\n", Mystr);
+return (0);
+}
