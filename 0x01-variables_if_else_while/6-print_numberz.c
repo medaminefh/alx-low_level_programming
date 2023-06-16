08c06a7b0261fbf9973904b4e0fc6329c6b312de
Mohamed Amine Fhal <fhal.mohamed.amine@gmail.com>
2023-06-15 21:51:03 -0700

    numbers with putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - This will convert ascii number into char
+(* a blank line
+ * Description: by placing %c instead of %d, the number will be converted)?
+(* section header: Section description)*
+ * Return: just return 0 for ok
+ */
+int main(void)
+{
+for (int a = 48; a < 58; a++)
+{
+putchar(a);
+}
+putchar('\n');
+return (0);
+}
