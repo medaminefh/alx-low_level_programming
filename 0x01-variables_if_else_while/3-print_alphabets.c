1e6629bb92fbb5ca12966a95f860f587038c9f21
Mohamed Amine Fhal <fhal.mohamed.amine@gmail.com>
2023-06-15 21:33:05 -0700

    all alphabets using the putchar fun

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - This will convert ascii number into char
+(* a blank line
+ * Description: by placing %c instead of %d, the number will be converted)?
+(* section header: Section description)*
+ * Return: just return 0 for ok
+ */
+int main(void)
+{
+int i = 65;
+int a = 97;
+for (a; a < 123; a++)
+{
+putchar(a);
+}
+for (i; i < 91 ; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
