8794940a2b502eb9a1a744e7cda2aff1b30cfcf2
Mohamed Amine Fhal <fhal.mohamed.amine@gmail.com>
2023-06-15 21:16:31 -0700

    last digit with the remainder

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - This will return a random num
+(* a blank line
+ * Description: This fun will print out a random num each time it runs)?
+(* section header: Section description)*
+ * Return: just return 0 for ok
+ */
+int main(void)
+{
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+int last_digit = n % 10;
+if (last_digit > 5)
+{
+printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
+}
+else if (last_digit < 6 && last_digit != 0)
+{
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
+}
+else
+printf("Last digit of %d is %d and is 0\n", n, last_digit);
+return (0);
+}
