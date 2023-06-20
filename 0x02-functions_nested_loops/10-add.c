cc8e3a5b50774fb01c4f4d979ac2d69b0b2a6248
Mohamed Amine Fhal <fhal.mohamed.amine@gmail.com>
2023-06-20 13:17:01 -0700

    adding numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * add - This will convert ascii number into char
+(* a blank line
+ * Description: by placing %c instead of %d, the number will be converted)?
+(* section header: Section description)*
+ * @a: is of type int
+ * @b: is of type int
+ * Return: just return 0 for ok
+ */
+
+int add(int a, int b)
+{
+int result = a + b;
+return (result);
+}
