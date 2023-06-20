122f590149dfb6d83ddded05ab1ff8ef7a7c27fa
Mohamed Amine Fhal <fhal.mohamed.amine@gmail.com>
2023-06-20 09:43:25 -0700

    times

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * times_table - This will convert ascii number into char
+(* a blank line
+ * Description: by placing %c instead of %d, the number will be converted)?
+(* section header: Section description)*
+ * Return: just return 0 for ok
+ */
+
+void times_table(void)
+{
+int b;
+int c;
+int d;
+
+for (b = 0; b <= 9; b++)
+{
+for (c = 0; c <= 9; c++)
+{
+d = c * b;
+if (d > 9)
+{
+_putchar(d / 10 + 48);
+_putchar(d % 10 + 48);
+_putchar(44);
+_putchar(' ');
+}
+else
+{
+_putchar((c * b) + 48);
+_putchar(44);
+_putchar(' ');
+}
+}
+_putchar('\n');
+}
+}
