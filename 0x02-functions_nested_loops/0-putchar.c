ebc2f05ca7d6a3454c5c12a1608e6e13eec616cd
Mohamed Amine Fhal <fhal.mohamed.amine@gmail.com>
2023-06-19 23:43:04 -0700

    using headers

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "header.h"
+/**
+ * main - This will convert ascii number into char
+(* a blank line
+ * Description: by placing %c instead of %d, the number will be converted)?
+(* section header: Section description)*
+ * Return: just return 0 for ok
+ */
+int main(void)
+{
+char out[8] = "_putchar";
+int i =0;
+while ( i < 8 )
+{
+_putchar(out[i]);
+i++;
+}
+_putchar('\n');
+return (0);
+}
