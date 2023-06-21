a7b219c6b54c311b1c0f4ffc4cd0a11275316fdd
Mohamed Amine Fhal <fhal.mohamed.amine@gmail.com>
2023-06-21 09:04:19 -0700

    natural numbers

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
-#include "main.h"
 /**
- * multiple - This will convert ascii number into char
+ * main - This will convert ascii number into char
 (* a blank line
  * Description: by placing %c instead of %d, the number will be converted)?
 (* section header: Section description)*
  * Return: just return 0 for ok
  */
 
-void multiple(void)
+int main(void)
 {
 int i;
 int sum = 23;
@@ -20,4 +19,5 @@ sum += i;
 }
 }
 printf("%d\n", sum);
+return (0);
 }
