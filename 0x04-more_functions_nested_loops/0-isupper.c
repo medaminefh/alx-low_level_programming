abb918934da045c8f38680b8888e48eddd4dd24b
Mohamed Amine Fhal <fhal.mohamed.amine@gmail.com>
2023-06-21 22:35:57 -0700

    upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isupper - This will convert ascii number into char
+(* a blank line
+ * Description: by placing %c instead of %d, the number will be converted)?
+(* section header: Section description)*
+ * @c: is an int
+ * Return: just return 0 for ok
+ */
+int _isupper(int c)
+{
+if (c >= 65 && c <= 90)
+{
+return (1);
+}
+return (0);
+}
