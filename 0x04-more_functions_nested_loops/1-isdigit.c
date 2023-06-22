fc0b1f0d0a11d08cefdd1c4a3480223382b12d02
Mohamed Amine Fhal <fhal.mohamed.amine@gmail.com>
2023-06-21 22:54:32 -0700

    is digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isdigit - This will convert ascii number into char
+(* a blank line
+ * Description: by placing %c instead of %d, the number will be converted)?
+(* section header: Section description)*
+ * @c: is an int
+ * Return: just return 0 for ok
+ */
+int _isdigit(int c)
+{
+if (c >= 48 && c <= 57)
+{
+return (1);
+}
+return (0);
+}
