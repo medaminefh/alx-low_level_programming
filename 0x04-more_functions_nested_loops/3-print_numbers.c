38965329bfacef99684420087a2a76d222d69fc6
Mohamed Amine Fhal <fhal.mohamed.amine@gmail.com>
2023-06-21 23:07:38 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * print_numbers - This will convert ascii number into char
+(* a blank line
+ * Description: by placing %c instead of %d, the number will be converted)?
+(* section header: Section description)*
+ * Return: just return 0 for ok
+ */
+void print_numbers(void)
+{
+int a = 48;
+while (a < 58)
+{
+putchar(a);
+a++;
+}
+putchar('\n');
+return (0);
+}
