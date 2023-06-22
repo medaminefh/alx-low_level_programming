e9849072adf9db2def3d8ddadd6e5a614134b262
Mohamed Amine Fhal <fhal.mohamed.amine@gmail.com>
2023-06-21 22:59:57 -0700

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - This will convert ascii number into char
+(* a blank line
+ * Description: by placing %c instead of %d, the number will be converted)?
+(* section header: Section description)*
+ * @a: is an int
+ * @b: is an int
+ * Return: just return 0 for ok
+ */
+int mul(int a, int b)
+{
+return  (a * b);
+}
