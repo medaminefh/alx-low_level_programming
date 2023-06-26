84f3e458d9822acec71375fb902a809b49024f72
Mohamed Amine Fhal <fhal.mohamed.amine@gmail.com>
2023-06-26 01:07:30 -0700

    reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * reset_to_98 - This will convert ascii number into char
+(* a blank line
+ * Description: by placing %c instead of %d, the number will be converted)?
+(* section header: Section description)*
+ * @n: is a pointer to an int
+ * Return: just return 0 for ok
+ */
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
