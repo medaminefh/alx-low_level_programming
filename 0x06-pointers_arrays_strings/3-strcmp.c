afd7384abe285a5c514ca5b62733aad720fef7fb
Mohamed Amine Fhal <fhal.mohamed.amine@gmail.com>
2023-06-29 02:53:11 -0700

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strcmp - This will convert ascii number into char
+(* a blank line
+ * Description: by placing %c instead of %d, the number will be converted)?
+(* section header: Section description)*
+ * @s1: is a pointer to a string
+ * @s2: is a pointer to a str
+ * Return: just return 0 for ok
+ */
+int _strcmp(char *s1, char *s2)
+{
+return (s1[0] - s2[0]);
+}
