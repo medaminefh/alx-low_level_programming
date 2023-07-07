ed3d5e9f93bedb65f6a66b11ad66469cd584e7f9
Mohamed Amine Fhal <fhal.mohamed.amine@gmail.com>
2023-07-07 13:48:29 -0700

    IDK

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,29 @@
-#include "main.h"
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <ctype.h>
+/**
+ * _isnum - This will convert ascii number into char
+(* a blank line
+ * Description: by placing %c instead of %d, the number will be converted)?
+(* section header: Section description)*
+ * @num: is a str
+ * Return: just return 0 for ok
+ */
+
+bool _isnum(char *num)
+{
+int len = strlen(num);
+int i;
+for (i = 0; i < len ; i++)
+{
+if (!isdigit(num[i]))
+	return (false);
+}
+return (true);
+}
+
 /**
  * main - This will convert ascii number into char
 (* a blank line
@@ -21,7 +44,7 @@ return (0);
 }
 for (i = 1; i < argc; i++)
 {
-if (atoi(argv[i]) == 0)
+if (!_isnum(argv[i]))
 {
 printf("Error\n");
 return (1);
