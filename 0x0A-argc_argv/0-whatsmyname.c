be9cf41e8531fde63fec77c2f78cf1bff5712dd1
Mohamed Amine Fhal <fhal.mohamed.amine@gmail.com>
2023-07-07 01:55:10 -0700

    file name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - This will convert ascii number into char
+(* a blank line
+ * Description: by placing %c instead of %d, the number will be converted)?
+(* section header: Section description)*
+ * @argc: is an int
+ * @argv: is an array of str
+ * Return: just return 0 for ok
+ */
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+printf("%s\n",argv[0]);
+return (0);
+}
