2b7b880e0e1a5c25019d0467b343b403a7f89add
Mohamed Amine Fhal <fhal.mohamed.amine@gmail.com>
2023-07-10 06:04:41 -0700

    free space

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -20,9 +20,24 @@ if (width <= 0 || height <= 0)
 	return (NULL);
 arr = malloc(sizeof(int *) * height);
 if (arr == NULL)
-	return (NULL);
+{
+free(arr);
+return (NULL);
+}
 for (i = 0; i < height; i++)
-	arr[i] = malloc(sizeof(int) * width);
+{
+arr[i] = malloc(sizeof(int) * width);
+if (arr[i] == NULL)
+{
+while (i > 0)
+{
+free(arr[i]);
+i--;
+}
+free(arr);
+return (NULL);
+}
+}
 for (i = 0; i < height; i++)
 {
 for (j = 0; j < width; j++)
