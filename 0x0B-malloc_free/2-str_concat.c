df4c4938787b24f24b7f03852332119fde80f9fd
Mohamed Amine Fhal <fhal.mohamed.amine@gmail.com>
2023-07-10 05:09:30 -0700

    concatstr

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * *str_concat - This will convert ascii number into char
+(* a blank line
+ * Description: by placing %c instead of %d, the number will be converted)?
+(* section header: Section description)*
+ * @s1: is a string
+ * @s2: is a string
+ * Return: just return 0 for ok
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+char *s;
+int i;
+int j;
+int a;
+int b;
+if (s1 != NULL)
+{
+for (i = 0; s1[i] != '\0'; i++)
+{}
+
+}
+if (s2 != NULL)
+{
+for (j = 0; s2[j] != '\0'; j++)
+{}
+s = malloc(sizeof(char) * (i + j) + 1);
+}
+if (s == NULL)
+	return (NULL);
+s[i + j + 1] = '\0';
+if (s1 != NULL)
+	for (a = 0; a < i; a++)
+		s[a] = s1[a];
+if (s2 != NULL)
+	for (b = 0; b < j; b++)
+		s[a + b] = s2[b];
+return (s);
+}
