731e09bcf3b62f3dba77e4fb9d082d915d6db76f
Mohamed Amine Fhal <fhal.mohamed.amine@gmail.com>
2023-07-11 06:36:40 -0700

    concat strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,55 +1,46 @@
-#include "main.h"
-#include <stdlib.h>
-#include <stdio.h>
+#include"main.h"
+#include<stdlib.h>
 
 /**
- * *str_concat - This will convert ascii number into char
-(* a blank line
- * Description: by placing %c instead of %d, the number will be converted)?
-(* section header: Section description)*
- * @s1: is a string
- * @s2: is a string
- * Return: just return 0 for ok
- */
+* *str_concat - This will convert ascii number into char
+(*a blank line
+* Description: by placing %c instead of %d, the number will be converted)?
+(*section header: Section description)*
+* @s1: is a string
+* @s2: is a string
+* Return: just return 0 for ok
+*/
 
 char *str_concat(char *s1, char *s2)
 {
+int i, j, a, b, len;
 char *s;
-int i;
-int j;
-int a = 0;
-int b = 0;
+a = b = 0;
+
 if (s1 != NULL)
 {
 i = 0;
-while (s1 && s1[i] != '\0')
-	i++;
+while (s1[i++] != '\0')
+	a++;
 }
+
 if (s2 != NULL)
 {
-j = 0;
-while (s2 && s2[j] != '\0')
-	j++;
+i = 0;
+while (s2[i++] != '\0')
+	b++;
 }
-s = malloc(sizeof(char) * (i + j) + 1);
+
+len = a + b;
+s = malloc(sizeof(char) * (len + 1));
 if (s == NULL)
 	return (NULL);
-if (s1 != NULL)
-{
-while (a < i)
-{
-s[a] = s1[a];
-a++;
-}
-}
-if (s2 != NULL)
-{
-while (b < j)
-{
-s[a + b] = s2[b];
-b++;
-}
-}
-s[i + j + 1] = '\0';
+
+for (i = 0; i < a; i++)
+	s[i] = s1[i];
+for (j = 0; j < b; j++, i++)
+	s[i] = s2[j];
+s[len] = '\0';
+
 return (s);
 }
