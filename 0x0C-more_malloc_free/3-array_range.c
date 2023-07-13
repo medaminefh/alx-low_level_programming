ca055fd377c8e130b329d6daa072616f52b2d00f
Mohamed Amine Fhal <fhal.mohamed.amine@gmail.com>
2023-07-13 01:29:31 -0700

    array of ints

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * *array_range - This will convert ascii number into char
+(* a blank line
+ * Description: by placing %c instead of %d, the number will be converted)?
+(* section header: Section description)*
+ * @min: is an int
+ * @max: is an int
+ * Return: just return 0 for ok
+ */
+int *array_range(int min, int max)
+{
+int i = 0;
+int *s;
+if (min > max)
+	return (NULL);
+
+s = malloc(sizeof(int) * (max - min));
+if (s == NULL)
+	return (NULL);
+while (min < max)
+{
+s[i] = min;
+min++;
+i++;
+}
+return (s);
+}
