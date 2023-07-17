5448396cc765b377c1165ab3f6231f72609c050d
Mohamed Amine Fhal <fhal.mohamed.amine@gmail.com>
2023-07-17 02:19:27 -0700

    macros

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - This will convert ascii number into char
+(* a blank line
+ * Description: by placing %c instead of %d, the number will be converted)?
+(* section header: Section description)*
+ * Return: just return 0 for ok
+ */
+int main(void)
+{
+printf("%s\n", __FILE__);
+return (0);
+}
