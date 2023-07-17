30d26b19f924770dcfa7bae348a5c4896615807c
Mohamed Amine Fhal <fhal.mohamed.amine@gmail.com>
2023-07-17 08:41:49 -0700

    dog struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,19 @@
+#ifndef DOG
+#define DOG
+
+/**
+ * struct dog - dog class
+ *
+ * @name: The dog's name
+ *
+ * @age: The dog's age
+ *
+ * @owner: The dog's owner
+ */
 struct dog {
 char *name;
 float age;
 char *owner;
 };
+
+#endif
