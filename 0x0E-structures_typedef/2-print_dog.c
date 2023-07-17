01d1131df2bbff37eb1779d1d227ac1692d39cde
Mohamed Amine Fhal <fhal.mohamed.amine@gmail.com>
2023-07-17 08:55:40 -0700

    print dog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * print_dog - This will convert ascii number into char
+(* a blank line
+ * Description: by placing %c instead of %d, the number will be converted)?
+(* section header: Section description)*
+ * @d: is of type dog
+ * Return: just return 0 for ok
+ */
+
+void print_dog(struct dog *d)
+{
+printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+}
