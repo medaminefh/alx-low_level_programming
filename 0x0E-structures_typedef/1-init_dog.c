55918f71e89e20a9ca8e269adaba51c3033c0d8f
Mohamed Amine Fhal <fhal.mohamed.amine@gmail.com>
2023-07-18 02:19:54 -0700

    fixes

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,5 @@
 #include "dog.h"
-
+#include <stddef.h>
 /**
  * init_dog - This will convert ascii number into char
 (* a blank line
@@ -14,7 +14,10 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+if (d != NULL)
+{
 d->name = name;
 d->age = age;
 d->owner = owner;
+}
 }
--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,5 +1,6 @@
 #include "dog.h"
 #include <stdio.h>
+#include <stddef.h>
 
 /**
  * print_dog - This will convert ascii number into char
@@ -12,6 +13,16 @@
 
 void print_dog(struct dog *d)
 {
-if (d)
-	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+if (d != NULL)
+{
+if (d->name != NULL)
+	printf("Name: %s\n", d->name);
+else
+	printf("Name: (nil)");
+printf("Age: %f\n", d->age);
+if (d->owner != NULL)
+	printf("Owner: %s\n", d->owner);
+else
+	printf("Owner: (nil)");
+}
 }
