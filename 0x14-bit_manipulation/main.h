450d4a61eff72d53f6ebfb436e98b78abba48fe3
Mohamed Amine Fhal <fhal.mohamed.amine@gmail.com>
2023-08-03 22:49:25 -0700

    add _putchar to main.h

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,6 +1,7 @@
 #ifndef MY_HEADER_FILE
 #define MY_HEADER_FILE
 
+int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
