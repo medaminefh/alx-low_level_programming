a312b015108c724286ad23a6236c2c61174a9f42
Mohamed Amine Fhal <fhal.mohamed.amine@gmail.com>
2023-08-07 11:54:07 -0700

    read text file

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,13 @@
+#ifndef MY_HEADER_FILE
+#define MY_HEADER_FILE
+
+#include <stddef.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif
