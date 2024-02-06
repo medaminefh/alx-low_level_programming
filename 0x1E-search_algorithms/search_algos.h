430c78be44ba74a8a4fa68020900eab2f0672a5b
Mohamed Amine Fhal <fhal.mohamed.amine@gmail.com>
2024-02-06 10:48:42 +0100

    linear search

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,12 @@
+/* Header file */
+#ifndef SEARCH_H
+#define SEARCH_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/* Function prototypes */
+int linear_search(int *array, size_t size, int value);
+
+#endif /* SEARCH_H */
